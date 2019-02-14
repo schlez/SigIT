64f8ed3ece729c9d3344528d12439e914b07d770
Schlesinger Rotem <rotemroy12@gmail.com>
2019-02-14 10:45:57 +0200

    feat: created main function

---
+++ ex3.py
@@ -0,0 +1,15 @@
+# the function gets a tic tac toe board and checks for wining or tie
+def check_win(board):
+    pass
+
+
+# The main function for the game board
+def main():
+    board = [[1, 2, 1],
+             [0, 1, 2],
+             [2, 0, 1]]
+    check_win(board)
+
+
+if __name__ == "main":
+    main()
