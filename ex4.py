6dd682b1ae301619c8cf5ad21d9ba032cd03f606
Schlesinger Rotem <rotemroy12@gmail.com>
2019-02-18 10:34:25 +0200

    feat: Created new simplified string compression function

--- ex4.py
+++ ex4.py
@@ -6,6 +6,22 @@ def compress(st1, i=0):
     return st1.replace((i + 1) * st1[i], st1[i] + str(i + 1))[:len(str(i + 1)) + 1] + (compress(st1[i + 1:]) if i < len(st1) - 1 else "")
 
 
+# Another option to solve this exercise in a simplified way
+# The function gets a string and compress it
+def string_compress(str1):
+    new_str, counter = "", 1  # declaring a new string to build the compressed string in it
+    # Loop all over the characters of the string
+    for i in range(0, len(str1)-1):
+        # Checks if there is a sequence of more than one character in a row
+        if str1[i] == str1[i + 1]:
+            counter += 1  # Count the sequence
+        else:  # In case that there is no sequence(the character appears one time in a row) or in case the sequence ends
+            new_str += str1[i] + str(counter)  # creating the new compressed string
+            counter = 1  # In order to initialize the sequence counter
+    new_str += str1[i] + str(counter)  # Compress the end of the string
+    return new_str
+
+
 def main():
     str = input("please enter a string: ")
     print(compress(str))
